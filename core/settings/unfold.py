73a580bee2597a246b720914f883005c2b821695
Abhimanue Rajesh <abhimanuemvk@gmail.com>
2026-07-06 16:06:13 +0530

    navigation bar update

--- core/settings/unfold.py
+++ core/settings/unfold.py
@@ -13,57 +13,28 @@ UNFOLD = {
         "show_search": False,
         "navigation": [
             {
-                "title": ("Overview"),
-                "collapsible": False,
+                "title": ("Navigation"),
                 "items": [
                     {
                         "title": ("Dashboard"),
                         "icon": "dashboard",
                         "link": reverse_lazy("admin:index"),
                     },
-                ],
-            },
-            {
-                "title": ("Task Management"),
-                "collapsible": False,
-                "separator": True,
-                "items": [
                     {
                         "title": ("Tasks"),
                         "icon": "task",
                         "link": reverse_lazy("admin:tasks_task_changelist"),
                     },
-                ],
-            },
-            {
-                "title": ("Ticketing"),
-                "collapsible": False,
-                "separator": True,
-                "items": [
                     {
                         "title": ("Support Tickets"),
                         "icon": "confirmation_number",
                         "link": reverse_lazy("admin:tickets_supportticket_changelist"),
                     },
-                ],
-            },
-            {
-                "title": ("Credentials"),
-                "collapsible": False,
-                "separator": True,
-                "items": [
                     {
                         "title": ("Credentials"),
                         "icon": "key",
                         "link": reverse_lazy("admin:credentials_credential_changelist"),
                     },
-                ],
-            },
-            {
-                "title": ("Quick Copy"),
-                "collapsible": False,
-                "separator": True,
-                "items": [
                     {
                         "title": ("Quick Copy Items"),
                         "icon": "content_copy",
@@ -73,7 +44,7 @@ UNFOLD = {
             },
             {
                 "title": ("Web Management"),
-                "collapsible": True,
+                "collapsible": False,
                 "separator": True,
                 "items": [
                     {
@@ -97,13 +68,6 @@ UNFOLD = {
                             "admin:web_management_webpagemanager_changelist"
                         ),
                     },
-                    # {
-                    #     "title": ("Web Forms"),
-                    #     "icon": "description",
-                    #     "link": reverse_lazy(
-                    #         "admin:web_management_webformmanager_changelist"
-                    #     ),
-                    # },
                     {
                         "title": ("DNS Zones"),
                         "icon": "article",
@@ -111,21 +75,6 @@ UNFOLD = {
                     },
                 ],
             },
-            # {
-            #     "title": ("Account"),
-            #     "collapsible": False,
-            #     "separator": True,
-            #     "items": [
-            #         {
-            #             "title": ("My Profile"),
-            #             "icon": "person",
-            #             "link": lambda request: reverse(
-            #                 "admin:auth_user_change",
-            #                 args=[request.user.id],
-            #             ),
-            #         },
-            #     ],
-            # },
         ],
     },
 }
